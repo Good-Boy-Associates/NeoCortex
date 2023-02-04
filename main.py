4591c70827e54d2bc89a68f596ef80f427919d84
SharonAliyas5573 <116248803+SharonAliyas5573@users.noreply.github.com>
2023-02-04 16:31:36 +0530

    Added all the frontend files

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder="static", template_folder="templates")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+app.run(debug=True)
